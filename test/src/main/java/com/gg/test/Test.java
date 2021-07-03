ab761d9e0da23e183aad29b17ab3cbf422db1c69
IanHu <1405656862@qq.com>
2021-07-03 19:32:20 +0800

    新增加了一个Test类

---
+++ test/src/main/java/com/gg/test/Test.java
@@ -0,0 +1,15 @@
+package com.gg.test;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName : Test
+ * @Description : 测试类
+ * @Author : Ian Hu
+ * @Date: 2021-07-03 19:30
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是哈哈");
+    }
+}
